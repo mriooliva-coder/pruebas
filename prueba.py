09dbc968d7022555c5fb68c97a367c52ada78ed3
mriooliva <mriooliva@gmail.com>
2026-03-01 21:53:29 -0600

    cambioss

--- prueba.py
+++ prueba.py
@@ -2,10 +2,5 @@ print("Hola")
 print("Como estas?")
 print(" todo bien ")
 
-<<<<<<< HEAD
 print("si!, todo bien en la casa y en el parque")
 print("y tu que?")
-=======
-print("si!, todo bien en el parque")
-print("y tu?")
->>>>>>> 36dd36f (cambioss)
