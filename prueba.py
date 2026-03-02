5df8fb35257a3e03984bcfd5af0a0a0cdf4d2614
mriooliva <mriooliva@gmail.com>
2026-03-01 21:49:07 -0600

    test1

--- prueba.py
+++ prueba.py
@@ -2,5 +2,10 @@ print("Hola")
 print("Como estas?")
 print(" todo bien ")
 
+<<<<<<< HEAD
 print("si!, todo bien en la casa y en el parque")
 print("y tu que?")
+=======
+print("si!, todo bien en el parque")
+print("y tu?")
+>>>>>>> 36dd36f (cambioss)
